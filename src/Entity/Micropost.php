ec7461ca33c77395182a8ce5dd47a91163b0be88
jpbenga <jpaulbenga@gmail.com>
2023-02-17 23:20:53 +0100

    form validation ok

--- src/Entity/Micropost.php
+++ src/Entity/Micropost.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\MicropostRepository;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MicropostRepository::class)]
 class Micropost
@@ -15,9 +16,13 @@ class Micropost
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 5, max: 255)]
     private ?string $title = null;
 
     #[ORM\Column(length: 500)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 5, max: 800)]
     private ?string $text = null;
 
    #[ORM\Column(type: 'datetime' , options:["default" => "CURRENT_TIMESTAMP"])]
